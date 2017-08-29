695d45672eca546ffafee96ff7432a091ed60799
Eric Sage <eric.david.sage@gmail.com>
2017-08-29 01:30:43 +0000

    Add Python 3 tag

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
     ],
     keywords='cytoscape networkx network biology cxmate',
     packages=find_packages(),
